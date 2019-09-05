09ff5fff70dd5662778588fb6da6c80e2efbbc24
ZP <goodzhao@gmail.com>
2019-09-05 16:12:05 -0400

    add rolling sum

--- Event Study/3_1_generate data for regression.R
+++ Event Study/3_1_generate data for regression.R
@@ -10,9 +10,10 @@ library(readstata13)
 library(tidyr)
 library(dplyr)
 library(lubridate)
-
+library(RcppRoll)
 
 setwd("U:/My Documents/R/Financial Analysis/Event Study")
+raw_data_folder <-"C:/Users/PZhao/Box/Effectiveness/Database/0. Raw data"
 data_folder <- "C:/Users/PZhao/Box/Effectiveness/Database/2. Database"
 saveFolder <- "C:/Users/PZhao/Box/Effectiveness/Database/4. R data"
 
@@ -30,7 +31,7 @@ load(paste(saveFolder, "panel data.rda", sep = "/"))
 main_df <- left_join(daily_df, df_main, by = c("date","ifs","embi","cds"))
 
 
-# aggregare multiple lenders into one
+# Aggregare multiple lenders into one
 event_df <- main_df%>%filter(bsl>0)
 short_event_df<- event_df%>%
     select(date,ifs,lender)%>%
@@ -45,43 +46,167 @@ short_event_df <-short_event_df%>%
 main_df <- left_join(main_df,short_event_df,by = c("date","ifs"))%>%
     filter(lender.x == lender.y | (is.na(lender.x) & is.na(lender.y)))
 
-### Generate Dummy for bsl t to t+4
-main_df <- main_df%>%arrange(ifs, date)%>%
-    mutate(bsl_dummy = bsl/bsl)%>%
-    group_by(ifs)%>%
-    mutate(bsl_dummy_lag = lag(bsl_dummy, 5)*2)%>%
+### Generate rolling sum of residual for t + 5 and t + 120, and t -20
+
+main_df <- main_df%>%group_by(ifs)%>%
+    arrange(date)%>%
+    mutate(e_embi_5d = roll_sum(e_embi, n = 5, fill = NA, align = "left"))%>%
+    mutate(e_embi_120d = roll_sum(e_embi, n = 120, fill = NA, align = "left"))%>%
+    mutate(e_cds_5d = roll_sum(e_cds, n = 5, fill = NA, align = "left"))%>%
+    mutate(e_cds_120d = roll_sum(e_cds, n = 120, fill = NA, align = "left"))%>%
+    mutate(e_embi_l_20d = roll_sum(e_embi, n = 20, fill = NA, align = "right"))%>%
+    mutate(e_cds_l_20d = roll_sum(e_cds, n = 20, fill = NA, align = "right"))%>%
     ungroup()
+
+# View(x%>%filter(year(date) >= 2010 & ifs == 536))
+
+### Kepp Dummy
 
 main_df <- main_df%>%
-    rowwise()%>%
-    mutate(range = sum(bsl_dummy, bsl_dummy_lag, na.rm =TRUE))%>%
-    ungroup()
+    arrange(ifs, date)%>%
+    mutate(bsl_dummy = bsl/bsl)
 
-### select only bsl_range is 1
-main_df[which(main_df$range==0),]$range<- NA
+main_df <- main_df%>%filter(bsl_dummy == 1)
+
+
+## Load GDP
+gdp_file <- "gdp.dta"
+gdp_df <- tbl_df(read.dta13((paste(data_folder, gdp_file, sep ='/'))))
+
+# merge with main dataframe
+main_df <- main_df%>%mutate(year = year(date))
+main_df <- left_join(main_df, gdp_df, by = c("ifs","year"))
+main_df%>%arrange(ifs,date)
+
+main_df <- main_df%>%mutate(size = bsl/gdp)
+
+## Generate Dummy for "First"
 
 main_df <- main_df%>%
     group_by(ifs)%>%
-    fill(range)%>%
-    ungroup()
+    arrange(date)%>%
+    mutate(is_first = row_number())
 
-main_df <- main_df%>%filter(range ==1)
+main_df[which(main_df$is_first>1),]$is_first <- 0
 
-main_df <- main_df%>%arrange(ifs,date)%>%
+## Generate Lagged Reserve to GDP
+
+# Load Reserve
+reserve_file <- "res.dta"
+reserve_df <- tbl_df(read.dta13((paste(data_folder, reserve_file, sep ='/'))))
+reserve_df <- left_join(reserve_df,gdp_df, by=c("ifs","year"))
+
+# Generate Lag
+reserve_df <- reserve_df%>%
+    mutate(res_gdp = res/gdp)%>%
     group_by(ifs)%>%
-    fill(event_id)%>%
+    arrange(year)%>%
+    mutate(res_gdp_lag = lag(res_gdp))%>%
     ungroup()
+# Merge with main dataframe
+reserve_df <- reserve_df%>%
+    select(ifs,year,res,res_gdp_lag)
 
-### Get sum of residuals for each event
-main_df <- main_df%>%group_by(event_id)%>%
-    summarise(date=min(date), ifs = first(ifs), cds = first(cds), embi = first(embi), china = first(china), 
-              e_embi_5d = sum(e_embi), e_cds_5d = sum(e_cds), vix = first(vix), us10 = first(us10),
-              lender = first(lender.x), borrower = first(borrower), bsl = first(bsl), all_lender = first(all_lender))
+main_df <- left_join(main_df,reserve_df, by = c("ifs","year"))
+main_df <- main_df%>%ungroup()
 
 
-View(main_df%>%filter(year(date) >= 2010 & ifs == 536))
+## Generate Lagged Reserve to M2
 
-### save
+# Load M2
+m2_file <- "m2.dta"
+m2_df <- tbl_df(read.dta13((paste(data_folder, m2_file, sep ='/'))))
+m2_df <- left_join(reserve_df,m2_df, by=c("ifs","year"))
+
+# Generate Lag
+m2_df <- m2_df%>%
+    mutate(res_m2= res/m2)%>%
+    group_by(ifs)%>%
+    arrange(year)%>%
+    mutate(res_m2_lag = lag(res_m2))%>%
+    ungroup()
+# Merge with main dataframe
+m2_df <- m2_df%>%
+    select(ifs,year,m2,res_m2_lag)
+
+main_df <- left_join(main_df,m2_df, by = c("ifs","year"))
+main_df <- main_df%>%ungroup()
+
+
+## Generate Lagged Short-term Debt to GDP
+
+# Load Short-term Debt
+debt_file <- "debt.dta"
+debt_df <- tbl_df(read.dta13((paste(data_folder, debt_file, sep ='/'))))
+debt_df <- left_join(gdp_df,debt_df, by=c("ifs","year"))
+
+# Generate Lag
+debt_df <- debt_df%>%
+    mutate(debt_gdp= debt/gdp)%>%
+    group_by(ifs)%>%
+    arrange(year)%>%
+    mutate(debt_gdp_lag = lag(debt_gdp))%>%
+    ungroup()
+# Merge with main dataframe
+debt_df <- debt_df%>%
+    select(ifs,year,debt,debt_gdp_lag)
+
+main_df <- left_join(main_df,debt_df, by = c("ifs","year"))
+main_df <- main_df%>%ungroup()
+
+## Generate Lagged Short-term Debt to M2
+
+main_df <- main_df%>%mutate(debt_m2 = debt/m2)%>%
+    group_by(ifs)%>%
+    arrange(year)%>%
+    mutate(debt_m2_lag = lag(debt_m2))%>%
+    ungroup()%>%
+    select(-debt_m2)
+
+## Load Program Dummy
+prog_file <- "program.xlsx"
+df_prog <- readxl::read_xlsx(path = paste(raw_data_folder, prog_file, sep = '/'))
+df_prog<- df_prog%>%select(countrycode, year, program)
+colnames(df_prog) <- c('ifs','year','program')
+main_df <- left_join(main_df,df_prog, by=c("ifs","year"))
+
+
+### Save Results
 hist(main_df$e_embi_5d)
 hist(main_df$e_cds_5d)
-save(main_df, file = paste(saveFolder, "event_data.Rda", sep ="/"))
+save(main_df, file = paste(saveFolder, "event_panel_data.Rda", sep ="/"))
+
+
+
+# ### Alternative code for generating Dummy for bsl t to t+4
+# main_df <- main_df%>%
+#     arrange(ifs, date)%>%
+#     mutate(bsl_dummy = bsl/bsl)%>%
+#     group_by(ifs)%>%
+#     mutate(bsl_dummy_lag = lag(bsl_dummy, 5)*2)%>%
+#     ungroup()
+# 
+# main_df <- main_df%>%
+#     mutate(range = rowSums(cbind(bsl_dummy, bsl_dummy_lag), na.rm =TRUE))%>%
+#     ungroup()
+# 
+# ### Select only bsl_range is 1
+# main_df[which(main_df$range==0),]$range<- NA
+# 
+# main_df <- main_df%>%
+#     group_by(ifs)%>%
+#     fill(range)%>%
+#     ungroup()
+# 
+# df_5d <- main_df%>%filter(range ==1)
+# 
+# main_df <- main_df%>%arrange(ifs,date)%>%
+#     group_by(ifs)%>%
+#     fill(event_id)%>%
+#     ungroup()
+# 
+# ### Get sum of residuals for each event
+# main_df <- main_df%>%group_by(event_id)%>%
+#     summarise(date=min(date), ifs = first(ifs), cds = first(cds), embi = first(embi), china = first(china), 
+#               e_embi_5d = sum(e_embi), e_cds_5d = sum(e_cds), vix = first(vix), us10 = first(us10),
+#               lender = first(lender.x), borrower = first(borrower), bsl = first(bsl), all_lender = first(all_lender))
